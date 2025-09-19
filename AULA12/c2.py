2154aa878bceee1e0ca248a27f17122bec3b9e98
karencristie <karen.cristie@hotmail.com>
2025-09-19 19:17:16 -0300

    Update c2.py

--- AULA12/c2.py
+++ AULA12/c2.py
@@ -15,3 +15,25 @@ byd.acelerar()
 #print(p2.nome)
 
 
+
+-------Outro exemplo
+class Carro:
+    def __init__(self, marca, ano):
+        self.marca = marca
+        self.ano = ano
+        
+    def ligar(self):
+        print(f"O carro {self.marca} está ligado."}
+              
+    def acelerar(self):
+         print(f"O carro {self.marca} está acelerando."}
+
+    ==Testando a classe==
+carro1 = Carro("Toyota", 2020)
+carro2 = Carro("Fiat", 2018)
+
+carro1.ligar()
+carro1.acelerar()
+
+carro2.ligar()
+carro2.acelerar()           
